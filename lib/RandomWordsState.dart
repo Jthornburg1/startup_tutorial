4f8341d28d3a98faf00e1f0b47dba117dabf1851
jonathan thornburg <reliable2192@gmail.com>
2018-04-24 17:40:37 -0400

    working app with table and detail screens

--- lib/RandomWordsState.dart
+++ lib/RandomWordsState.dart
@@ -63,29 +63,32 @@ class RandomWordsState extends State<RandomWords> {
   void _pushSaved() {
     Navigator.of(context).push(
       new MaterialPageRoute(
-          builder: (context) {
-            final tiles = _saved.map(
+        builder: (context) {
+          final tiles = _saved.map(
                 (pair) {
-                  return new ListTile(
-                    title: new Text(
-                      pair.asPascalCase,
-                      style: _biggerFont,
-                    ),
-                  );
-                }
-            );
-            final divided = ListTile
-            .divideTiles(
-                tiles: tiles)
-            .toList();
-            return new Scaffold(
-              appBar: new AppBar(
-                title: new Text('Saved Suggestions'),
-              ),
-              body: new ListView(children: divided,),
-            );
-          }
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
           )
+              .toList();
+
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          );
+        },
+      ),
     );
   }
 }
